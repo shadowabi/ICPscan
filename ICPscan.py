f48da60f0c065c6b9ef8c81f53024b8d10fd07f2
Sh4d0w_小白 <50265741+914525753@users.noreply.github.com>
2021-11-27 22:27:32 +0800

    Add files via upload
    解决线程死锁问题，新增进度条提示

--- ICPscan.py
+++ ICPscan.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 #author:Sh4d0w_小白
 
-from requests import get
+from requests import get,Session
 from sys import argv
 from queue import Queue
 from threading import Thread
@@ -11,39 +11,50 @@ from re import *
 from bs4 import BeautifulSoup
 from lxml import etree
 import os
-from fake_useragent import UserAgent
+# from fake_useragent import UserAgent
+# from requests.adapters import HTTPAdapter
+from tqdm import tqdm
+import urllib3
 
 q = Queue()
-
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 icp = "https://icplishi.com/"
 flag = 0
-ua = UserAgent()
+# ua = UserAgent()
 result = []
 number = ""
+tasknumber = 0
 
 header = {
-	"User-Agent":ua.random
+	"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4621.0 Safari/537.36"
 }
+
+# session = Session()
+# session.mount('http://', HTTPAdapter(max_retries=2))
+# session.mount('https://', HTTPAdapter(max_retries=2))
 
 def Usage():
 	print("[Usage]python ICPscan.py (domian/ICP)")
 
 def Output():
-	global result
+	global result,pbar
 	while(q.empty() == False):
-		url = "https://" + q.get()
+		url = "http://" + q.get()
 		try:
-			r = get(url = url, headers = header, allow_redirects = True, timeout = 1)
-			print(url)
+			r = get(url = url, headers = header, allow_redirects = True, timeout = 1, verify = False)
+			r.raise_for_status()
+			# print(url)
 			result.append(url.strip())
 		except:
 			pass
 		finally:
 			q.task_done()
+			pbar.update(1)
 		
 
 
 def GetDomain(html):
+	global tasknumber
 	soup = BeautifulSoup(html,'lxml')
 	text = str(soup.find_all("td"))
 	text = etree.HTML(text = text)
@@ -51,6 +62,7 @@ def GetDomain(html):
 	text = compile(r'.*\w+\.\w+').findall(text)
 	for i in text:
 		q.put(i)
+		tasknumber += 1
 
 def Check(url):
 	global flag,number
@@ -94,7 +106,7 @@ def Scan(url):
 	url = Check(url)
 	url = icp + url
 	try:
-		r = get(url = url,headers = header, allow_redirects = True, timeout = 5)
+		r = get(url = url,headers = header, allow_redirects = True, timeout = 2, verify = False)
 		r.raise_for_status()
 	except:
 		print("连接超时，请稍后重试")
@@ -104,7 +116,7 @@ def Scan(url):
 			icp2 = GetICP(r.text)
 			url = icp + icp2
 			try:
-				r = get(url = url,headers = header, allow_redirects = True, timeout = 5)
+				r = get(url = url,headers = header, allow_redirects = True, timeout = 2, verify = False)
 				r.raise_for_status()
 			except:
 				print("连接超时，请稍后重试")
@@ -116,20 +128,27 @@ def Scan(url):
 
 
 if __name__=="__main__":
+	global pbar
 	try:
 		scan = argv[1]
 		print("开始进行扫描：")
 		Scan(scan)
+		pbar = tqdm(total = tasknumber)
 		for i in range(20):
 			t = Thread(target = Output)
 			sleep(0.1)
 			t.start()
-			t.join()
-		print("扫描结束！正在写入到文件result.txt中")
+			t.join(1)
+		pbar.close()
+		os.system("cls")
+		print("扫描结束！输出结果")
 		with open("result.txt","w+",encoding='utf8') as f:
 			f.write(number + "\n")
 			for i in result:
+				print(i)
 				f.write(i + "\n")
-		print("写入完毕，程序退出")
+		print("已保存到result.txt文件中，按回车键退出程序！")
+		input()
+		os._exit(0)
 	except:
 		Usage()
