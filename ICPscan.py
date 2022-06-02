1004023a70fca25c5f8b56a31534de78e3d272f8
Sh4d0w_小白 <50265741+914525753@users.noreply.github.com>
2022-06-02 17:44:28 +0800

    优化扫描方式，新增进度条，改善程序逻辑

--- ICPscan.py
+++ ICPscan.py
@@ -2,155 +2,111 @@
 # -*- coding:utf-8 -*-
 #author:Sh4d0w_小白
 
-from requests import get,Session
-from sys import argv
-from queue import Queue
-from threading import Thread
-from time import sleep
+import grequests
+import argparse
+from requests import get
 from re import *
 from bs4 import BeautifulSoup
 from lxml import etree
-import os
-# from fake_useragent import UserAgent
-# from requests.adapters import HTTPAdapter
+from os import system, _exit
 import platform
-from tqdm import tqdm
 import urllib3
+from tqdm import tqdm
 
-q = Queue()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+ap = argparse.ArgumentParser()
+ap.add_argument("-u", "--url", help = "Input IP/DOMAIN/URL/ICP", metavar = "www.baidu.com", required = True)
 icp = "https://icplishi.com/"
 flag = 0
-# ua = UserAgent()
-result = []
-number = ""
-tasknumber = 0
+ICPnumber = ""
+rs = []
+good = []
 
 header = {
 	"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4621.0 Safari/537.36"
 }
 
-# session = Session()
-# session.mount('http://', HTTPAdapter(max_retries=2))
-# session.mount('https://', HTTPAdapter(max_retries=2))
-
-def Usage():
-	print("[Usage]python ICPscan.py (domian/ICP)")
-
-def Output():
-	global result,pbar
-	while(q.empty() == False):
-		url = "http://" + q.get()
-		try:
-			r = get(url = url, headers = header, allow_redirects = True, timeout = 1, verify = False)
-			r.raise_for_status()
-			# print(url)
-			result.append(url.strip())
-		except:
-			pass
-		finally:
-			q.task_done()
-			pbar.update(1)
-		
-
+def clear():
+	if(platform.system() == "Windows"):
+		system("cls")
+	else:
+		system("clear")	
 
 def GetDomain(html):
-	global tasknumber
 	soup = BeautifulSoup(html,'lxml')
-	text = str(soup.find_all("td"))
+	text = str(soup.select("body > div > div.container > div > div.module.mod-panel > div > div:nth-child(2) > div.c-bd > div"))
 	text = etree.HTML(text = text)
 	text = text.xpath('string(.)')
 	text = compile(r'.*\w+\.\w+').findall(text)
 	for i in text:
-		q.put(i)
-		tasknumber += 1
-
-def Check(url):
-	global flag,number
-	if(search(r"(http|https)\:\/\/", url)):
-		url = (sub(r"(http|https)\:\/\/", "", url)) #去除协议头
-		flag = 1 #域名
-		return(url)
-	elif(search(r".ICP.\d{6}.-\d{1,}",url)):
-		url = sub("-\d{1,}", "", url) #提取主备案号
-		number = url
-		flag = 2 #备案号
-		return(url)
-	elif(search(r".*\w+\.\w+",url)):
-		flag = 1 #域名
-		return(url)
-	elif(search(r".ICP.\d{6}.",url)):
-		number = url
-		flag = 2 #备案号
-		return(url)
-	else:
-		print("请检查输入是否正确")
-		os._exit(0)
+		try:
+			rs.append(grequests.get("http://" + i, headers = header, timeout = 3, verify = False)) #扫描
+		except:
+			pass
+	for i in grequests.map(rs):
+		if i.status_code == 200:
+			good.append(i.url)
 
 
 def GetICP(html):
-	global number
+	global ICPnumber
 	soup = BeautifulSoup(html,'lxml')
 	text = str(soup.select("body > div > div.container > div > div.module.mod-panel > div.bd > div:nth-child(2) > div.c-bd"))
 	text = etree.HTML(text = text)
 	text = text.xpath('string(.)')
-	text = search(r".ICP.\d{6}.", text) #获取备案号
+	text = search(r".ICP.\d{6,}.", text) #获取备案号
 	if (text != None):
-		number = text.group()
-		return text.group()
-	else:
-		print("请检查输入是否正确")
-		os._exit(0)
+		ICPnumber = text.group()
+
+def Check(url):
+	global flag,ICPnumber
+	if(search(r"(http|https)\:\/\/", url)): # 当输入URL时提取出域名
+	    url = sub(r"(http|https)\:\/\/", "", url)
+	    if (search(r"(\/|\\).*", url)):
+	        url = sub(r"(\/|\\).*", "", url)
+	
+	if match(r"^([a-zA-Z0-9]([a-zA-Z0-9-_]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,11}$", url): #匹配域名
+		flag = 1 #域名
+		return url
+
+	if(search(r".ICP.\d{6,}.",url)):
+		if(search(r".ICP.\d{6,}.-\d{1,}",url)):
+			url = sub("-\d{1,}", "", url) #提取主备案号
+		ICPnumber = url
+		flag = 2 #备案号
+		return url
 
 def Scan(url):
-	global icp
-	url = Check(url)
-	url = icp + url
 	try:
-		r = get(url = url,headers = header, allow_redirects = True, timeout = 2, verify = False)
-		r.raise_for_status()
-	except:
-		print("连接超时，请稍后重试")
-		os._exit(0)
-	finally:
+		url = Check(url)
+		if flag == 0:
+			raise Exception("请检查输入是否正确")
+		url = icp + url
+		r = get(url = url, headers = header, timeout = 2, verify = False)
 		if(flag == 1):
-			icp2 = GetICP(r.text)
-			url = icp + icp2
-			try:
-				r = get(url = url,headers = header, allow_redirects = True, timeout = 2, verify = False)
-				r.raise_for_status()
-			except:
-				print("连接超时，请稍后重试")
-				os._exit(0)
-			sleep(0.1)
-			GetDomain(r.text)
-		elif(flag == 2):
-			GetDomain(r.text)
+			GetICP(r.text) #通过域名提取ICP号
+			if ICPnumber == "":
+				raise Exception("该域名无法提取备案号")
+		url = icp + ICPnumber
+		r = get(url = url, headers = header, timeout = 2, verify = False)
+		GetDomain(r.text)
+	except Exception as err:
+		print(err)
 
 
 if __name__=="__main__":
-	global pbar
-	try:
-		scan = argv[1]
-		print("开始进行扫描：")
-		Scan(scan)
-		pbar = tqdm(total = tasknumber)
-		for i in range(20):
-			t = Thread(target = Output)
-			sleep(0.1)
-			t.start()
-			t.join(1)
-		pbar.close()
-		cmd = "cls" if platform.system() == "Windows" else "clear"
-		os.system(cmd)
-		print("扫描结束！输出结果")
-		with open("result.txt","w+",encoding='utf8') as f:
-			f.write(number + "\n")
-			for i in result:
-				print(i)
-				f.write(i + "\n")
-		print("已保存到result.txt文件中，按回车键退出程序！")
-		input()
-		os._exit(0)
-	except:
-		Usage()
+	args = ap.parse_args()
+	url = args.url
+	print("开始进行扫描：")
+	Scan(url)
+	clear()
+	print("扫描结束！")
+	with open("result.txt","w+",encoding='utf8') as f:
+		f.write(ICPnumber + "\n")
+		print(ICPnumber)
+		for i in good:
+			print(i)
+			f.write(i + "\n")
+	print("已保存到result.txt文件中，按回车键退出程序！")
+	input()
+	_exit(0)
